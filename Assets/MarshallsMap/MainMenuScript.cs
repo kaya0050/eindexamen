e8582acc4c5ec7af68996589bb2dd02fb9f938b8
marshall verboekend <marshalllyron@gmail.com>
2026-05-13 10:19:32 +0200

    Main menu functionaliteit

---
+++ Assets/MarshallsMap/MainMenuScript.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject optionMenu;
+    public GameObject statsMenu;
+    public GameObject playerSelectMenu;
+
+    public void OpenOptionsMenu()
+    {
+        // deze functie word aan een button gekoppeld zodat je het options menu kan openen.
+        optionMenu.SetActive(true);
+    }
+
+    public void CloseOptionsMenu()
+    {
+        // deze functie word aan een button gekoppeld zodat je het options menu kan sluiten.
+        optionMenu.SetActive(false);
+    }
+
+    public void OpenStatsMenu()
+    {
+        // deze functie word aan een button gekoppeld zodat je het stats menu kan openen.
+        statsMenu.SetActive(true);
+    }
+
+    public void CloseStatsMenu()
+    {
+        // deze functie word aan een button gekoppeld zodat je het stats menu kan sluiten.
+        statsMenu.SetActive(false);
+    }
+
+    public void OpenPlayerSelectMenu()
+    {
+        // deze functie word aan een button gekoppeld zodat je het player select menu kan openen.
+        playerSelectMenu.SetActive(true);
+    }
+
+    public void ClosePlayerSelectMenu()
+    {
+        // deze functie word aan een button gekoppeld zodat je het player select menu kan sluiten.
+        playerSelectMenu.SetActive(false);
+    }
+
+    public void StartGame2Player()
+    {
+        // deze functie word aan een button gekoppeld en zorgt dat er 2 spelers in de scene zijn om mee te spelen.
+        //SceneManager.LoadScene("GameScene");
+    }
+
+    public void StartGame3Player()
+    {
+        // deze functie word aan een button gekoppeld en zorgt dat er 3 spelers in de scene zijn om mee te spelen.
+        //SceneManager.LoadScene("GameScene");
+    }
+
+    public void StartGame4Player()
+    {
+        // deze functie word aan een button gekoppeld en zorgt dat er 4 spelers in de scene zijn om mee te spelen.
+        //SceneManager.LoadScene("GameScene");
+    }
+
+    public void CloseGame()
+    {
+        // deze functie word aan een button gekoppeld en sluit het spel af als deze button gebruikt word.
+        Environment.Exit(0);
+    }
+}
