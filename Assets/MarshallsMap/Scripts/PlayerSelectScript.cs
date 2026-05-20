d7d8659a87be492d3c4baa8fc402643a33a7ffe5
marshall verboekend <marshalllyron@gmail.com>
2026-05-20 14:09:31 +0200

    character select screen

--- Assets/MarshallsMap/Scripts/PlayerSelectScript.cs
+++ Assets/MarshallsMap/Scripts/PlayerSelectScript.cs
@@ -1,5 +1,8 @@
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerSelectScript : MonoBehaviour
 {
@@ -15,38 +18,52 @@ public class PlayerSelectScript : MonoBehaviour
 
         [HideInInspector]
         public bool ready;
+
+        public TextMeshProUGUI readyText;
+        public TextMeshProUGUI joinText;
+
+        public Button readyButton;
+        public Button nextButton;
+        public Button previousButton;
     }
 
     public PlayerSlot[] players;
 
-    public int currentPlayerAmount = 2;
+    public int currentPlayerAmount;
 
     public GameObject startButton;
 
-    void Start()
+    void Update()
     {
+        bool allReady = false;
+
+        if (currentPlayerAmount > 1)
+        {
+            allReady = true;
+
+            for (int i = 0; i < currentPlayerAmount; i++)
+            {
+                if (!players[i].ready)
+                {
+                    allReady = false;
+                }
+            }
+        }
+
         for (int i = 0; i < players.Length; i++)
         {
             bool active = i < currentPlayerAmount;
 
-            players[i].playerPanel.SetActive(active);
+            for (int j = 0; j < players[i].characterModels.Length; j++)
+            {
+                players[i].characterModels[j].SetActive(active);
+                players[i].joinText.gameObject.SetActive(!active);
+                players[i].readyButton.gameObject.SetActive(active);
+            }
 
             if (active)
             {
                 ShowCharacter(i);
-            }
-        }
-    }
-
-    void Update()
-    {
-        bool allReady = true;
-
-        for (int i = 0; i < currentPlayerAmount; i++)
-        {
-            if (!players[i].ready)
-            {
-                allReady = false;
             }
         }
 
@@ -81,6 +98,18 @@ public class PlayerSelectScript : MonoBehaviour
     public void ToggleReady(int playerIndex)
     {
         players[playerIndex].ready = !players[playerIndex].ready;
+
+        if (players[playerIndex].ready)
+        {
+            players[playerIndex].readyText.text = "Ready";
+            players[playerIndex].nextButton.gameObject.SetActive(false);
+            players[playerIndex].previousButton.gameObject.SetActive(false);
+        } else
+        {
+            players[playerIndex].readyText.text = "Not ready";
+            players[playerIndex].nextButton.gameObject.SetActive(true);
+            players[playerIndex].previousButton.gameObject.SetActive(true);
+        }
     }
 
     void ShowCharacter(int playerIndex)
