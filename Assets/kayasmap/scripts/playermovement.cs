bde0279d3fdd6ca021d156dd4b3d3e9eb1db78f1
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2026-05-28 13:01:48 +0200

    main menu heeft nu het roodkapje speler model

--- Assets/kayasmap/scripts/playermovement.cs
+++ Assets/kayasmap/scripts/playermovement.cs
@@ -1,3 +1,5 @@
+using Unity.Hierarchy;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -36,13 +38,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        
         Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
 
         if (movement.magnitude > 1f)
         {
             movement.Normalize();
         }
-
+        //rotate
+        if (movement.x != 0 || movement.z != 0)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(movement.x, 0, movement.z));
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+        }
         // gravity
         if (characterController.isGrounded && yVelocity < 0)
         {
